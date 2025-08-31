a75c8d000e04b33aacd4693ed962aa6b7158d44f
praloved <pralove.dixit@gmail.com>
2025-08-31 20:16:28 +0530

    added for external dependencies

--- python-demo/app/hello.py
+++ python-demo/app/hello.py
@@ -1,11 +1,13 @@
+import requests
 from utils import math
 
 
-
 def main():
-    result = math.add(4, 5)
-    print("Hello, Bazel!! 4+5=", result)
+    result = math.add(2, 3)
+    r = requests.get("https://httpbin.org/get")
+    print("Hello, Bazel! 2 + 3 =", result)
+    print("Fetched status code:", r.status_code)
+
 
 if __name__ == "__main__":
     main()
-
